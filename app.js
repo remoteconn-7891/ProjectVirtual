1151f73f738c4a958906913621f42459cf5ff954
corey james <coreyjames689@aol.com>
2023-12-25 11:44:29 -0700

    Update on routers using middelweare for both sign up and login screens

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express()
 
-const port = 3050;
+const port = 3060;
 
+function loginUser(req, res, next){
+    //retrieve info from req object
+    res.locals.loggedIn = true
+    console.log("Successful login!")
+    next();
+}
 
-// Route (API) for login with Express.js using HTTP Req & Res (POST)
+app.use('/login',loginUser);
+
+//Router for user registering an account with middleware
+app.post('/register', (req, res, next) => {
+
+});
+
+// Router (API) for login in Express.js using HTTP Req & Res (POST)
 app.post('/login', (req, res, next) => {
+
     if (email) {
         // Successful response (200) code
         res.status(200);
@@ -25,7 +39,7 @@ app.post('/login', (req, res, next) => {
     }
 
     res.send('Successful login');
-    next();
+    console.log(res.locals.loggedIn)
 
     const loggedIn = {
         email: 'email@example.com',
