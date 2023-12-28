49e123e7823670e6f6555bc896c2e0db70fca612
corey james <coreyjames689@aol.com>
2023-12-28 12:33:05 -0700

    Login route middleware revised

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const app = express();
 
 const port = 3060;
 
-app.use('/login',loginUser);
-
 //Router for user registering an account with middleware
 app.post('/register', (req, res, next) => {
 
@@ -13,39 +11,13 @@ app.post('/register', (req, res, next) => {
 // Router (API) for login in Express.js using HTTP Req & Res (POST)
 app.post('/login', (req, res, next) => {
 
-    if (email) {
-        // Successful response (200) code
-        res.status(200);
-    } else {
-        // Error status (401) code
-        res.status(401), {
-            error: 'invalid email',
-        }
-    }
-
-    if (password) {
-        res.status(200);
-    } else {
-        res.status(401), {
-            error: 'Invalid password',
-        }
-    }
-
-    res.send('Successful login');
-    console.log(res.locals.loggedIn)
-
-    const loggedIn = {
-        email: 'email@example.com',
-        password: 'Password!123', 
-    };
+    const email = 'example@email.com'
+    const password = 'Password123!'
+    
+    if(!email) res.status(401).send('Invalid email')
+    if(!password) res.status(401).send('Invalid password')
+    res.status(200).send('Successful login')
 });
-
-function loginUser(req, res, next){
-    //retrieve info from req object
-    res.locals.loggedIn = true
-    console.log("Successful login!")
-    next();
-}
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}..`);
