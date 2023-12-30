3b3172e09c8518b5748e49cae9c3d5911610e1f9
corey james <coreyjames689@aol.com>
2023-12-30 13:27:42 -0700

    .env update to project Virtualify

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const app = express();
 const port = 3060;
 
 //Router for user registering an account with middleware
-app.post('/register', (req, res, next) => {
+app.post('/register', (req, res,) => {
 
 });
 
 // Router (API) for login in Express.js using HTTP Req & Res (POST)
-app.post('/login', (req, res, next) => {
+app.post('/login', (req, res,) => {
 
     const email = 'example@email.com'
     const password = 'Password123!'
