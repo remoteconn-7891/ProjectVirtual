07cec14c5b4e53cc919b794629d1a41ab624205e
corey james <coreyjames689@aol.com>
2023-12-31 02:10:48 -0700

    Updated server.js by adding new router for logout

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const app = express();
 const port = 3060;
 
 //Router for user registering an account with middleware
-app.post('/register', (req, res,) => {
+app.post('/register', (req, res) => {
+    res.status(200).send('Successfully created an account')
 
 });
 
 // Router (API) for login in Express.js using HTTP Req & Res (POST)
-app.post('/login', (req, res,) => {
+app.post('/login', (req, res) => {
 
     const email = 'example@email.com'
     const password = 'Password123!'
@@ -17,6 +18,12 @@ app.post('/login', (req, res,) => {
     if(!email) res.status(401).send('Invalid email')
     if(!password) res.status(401).send('Invalid password')
     res.status(200).send('Successful login')
+});
+
+// Router for logging out user
+app.post('/logout', (req, res) => {
+    res.status(200).send('Successfully logged out')
+    
 });
 
 app.listen(port, () => {
